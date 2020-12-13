71580c083d6c20b0683eced15ce9e1485f6eb5a7
ling2e <7447468-ladias@users.noreply.gitlab.com>
2020-12-13 15:27:45 +0800

    update

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -22,7 +22,7 @@
 
 <!-- Tab panes -->
 <div class="tab-content">
-    <div class="tab-pane fade show active" id="tab1Id" role="tabpanel"></div>
+    <div class="tab-pane fade show active" id="tab1Id" role="tabpanel">asdasd</div>
     <div class="tab-pane fade" id="tab2Id" role="tabpanel"></div>
     <div class="tab-pane fade" id="tab3Id" role="tabpanel"></div>
     <div class="tab-pane fade" id="tab4Id" role="tabpanel"></div>
