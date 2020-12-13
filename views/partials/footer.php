7514341e041619d1488adc53b2004e4390e0d4ed
ling2e <7447468-ladias@users.noreply.gitlab.com>
2020-12-13 10:09:21 +0800

    setup update

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,3 +1,3 @@
-<footer class="bg-dark text-center">
-    <small><i class="fas fa-copyright"></i>2020-2022, ??.com, Inc. or its affiliates</small>
+<footer class="bg-dark text-secondary py-4 text-center">
+    <small><i class="fas fa-copyright"></i> 2020-2022, ??.com, Inc. or its affiliates</small>
 </footer>
--- views/partials/layout.php
+++ views/partials/layout.php
@@ -13,6 +13,9 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous" defer></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous" defer></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous" defer></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,300;0,500;1,700&display=swap" rel="stylesheet">
 
     <!-- Add the script and css file under here -->
     <link rel="stylesheet" href="/assets/css/style.css">
@@ -23,7 +26,7 @@
       
     <?
         include "nav.php";
-        
+
         echo "<main>";
         get_content();
         echo "</main>";
