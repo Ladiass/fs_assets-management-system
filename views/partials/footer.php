22e4054cf5efe5ed5b133c7fcf770547df6bd044
ling2e <7447468-ladias@users.noreply.gitlab.com>
2020-12-15 16:16:50 +0800

    new and nice footer

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,3 +1,3 @@
 <footer class="bg-dark text-white py-4 text-center footer">
-	<small>For educational purposes only.</small>
+<small><i class="fas fa-copyright"></i> 2020-2022, S.E.C.com, Inc. or its affiliates</small>
 </footer>
